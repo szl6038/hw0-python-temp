e159b2e634714a4f69c160f7d498cbc22ac8bcd4
szl6038 <szl6038@psu.edu>
2020-08-26 20:30:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+temp = input("Enter temperature in Celsius: ")
+
+tem = float(temp)
+
+fahr = (tem * 1.8 + 32)
+
+fah = str(fahr)
+
+print((temp) + "° in Celsius is equivalent to " + (fah) + "° Fahrenheit.")
